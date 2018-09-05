85f5ba897e073749b151ab354c7054cfe9af4de9
Zepper <zeppertrek@gmail.com>
2018-09-05 12:47:15 +0530

    Threadpool changes

--- src/sandpit/ThreadPool.java
+++ src/sandpit/ThreadPool.java
@@ -3,8 +3,9 @@ package sandpit;
 public class ThreadPool {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// By default, evet java app will have 1 thread spawned by main()
+		 Thread t = Thread.currentThread();
+		 System.out.println("Current thread: " + t);
 	}
 
 }
